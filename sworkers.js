a70b204f97f2071fe55f6aeaa12c08366e1edafd
Noel Cruz <noelll361422@gmail.com>
2024-01-09 22:35:14 -0300

    1

---
+++ sworkers.js
@@ -0,0 +1,34 @@
+let cacheName='cachegestor'
+let appArchivos=[
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js'
+];
+
+
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(appArchivos);
+    }),
+  );
+});
+
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    }),
+  );
+});
