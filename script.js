cc1475ecd6fe58c4354659038670ca7b88174513
Noel-Cruz <126811536+Noel-Cruz@users.noreply.github.com>
2024-01-15 13:04:08 -0300

    91

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ span.onclick=(e)=>{
     // e.stopPropagation()
     // console.log(e);
 };
-sombra.onclick=(e)=>{
+sombra.ontouchstart=(e)=>{
     console.log(e);
     e.preventDefault();
     const som=document.getElementById('som');
