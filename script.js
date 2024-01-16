59b7913717c977dcf088b191b94ab9a34488913e
Noel Cruz <noelll361422@gmail.com>
2024-01-16 01:24:33 -0300

    huella-de-tacto-V1

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 const sombra=document.getElementById('sombra');
-// sombra.onclick=(e)=>{
-//     console.dir(e);
-//     e.preventDefault();
-//     let i=38;
-// let o=setInterval(() => {
-//         sombra.style.backgroundImage=`radial-gradient(circle ${i}px at ${e.offsetX}px ${e.offsetY}px, rgb(255 255 255 / 15%) 80%,transparent)`;
-//         i==120&&bi();
-//         i++;
-//         function bi(){
-//             clearInterval(o);
-//             sombra.style.backgroundImage='none';
-//         };
-//     },0.01);
-
-// };
 const span=document.getElementById('span');
-span.onclick=(e)=>{
-    // e.stopPropagation()
-    // console.log(e);
-};
 sombra.onclick=(e)=>{
     console.log(e);
     e.preventDefault();
@@ -29,8 +10,8 @@ sombra.onclick=(e)=>{
     const y=e.offsetY-8;
     som.style.left=`${x}px`;
     som.style.top=`${y}px`;
-    som.style.display='block';
+    som.style.display='none';
     setTimeout(() => {
-        som.style.display='none'; 
-    }, 400);
+        som.style.display='block';
+    }, 0);
 };
